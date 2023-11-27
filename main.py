7b126d84f0aca5674943912a76dc80c43c1b9527
Mac Nisbet <mfnisbet@lakeheadu.ca>
2023-11-27 16:40:27 -0500

    Added register + reset
    Added a function to register new users (including email), and a function to reset 2FA if they lose their app. This simulates an email server, to reset the user just enters their username and their associated email addres.

--- main.py
+++ main.py
@@ -22,11 +22,16 @@ class UserLoginSystem:
         self.password_label = tk.Label(self.window, text="Password:")
         self.password_entry = tk.Entry(self.window, show="*")
         self.login_button = tk.Button(self.window, text="Login", command=self.login)
+        self.register_button = tk.Button(self.window, text="Register", command=self.create_user)
+        self.generate_key_button = tk.Button(self.window, text="Reset 2FA", command=self.generate_and_display_key)
+        
         self.username_label.pack()
         self.username_entry.pack()
         self.password_label.pack()
         self.password_entry.pack()
         self.login_button.pack()
+        self.register_button.pack()
+        self.generate_key_button.pack()
 
         # Use this to change window dimensions
         self.window.geometry("400x350")
@@ -47,9 +52,7 @@ class UserLoginSystem:
                 if 'error' not in response:
                     otp_secret = response['secret_key']
                     current_code = response['current_code']
-                    # Display the QR code
-                    self.generate_qr_code(otp_secret)
-                    # Then, prompt for OTP
+                    # Don't display the QR code, should only be displayed on user creation or email
                     entered_otp = simpledialog.askstring("OTP", "Enter OTP:")
                     if entered_otp and self.verify_otp(otp_secret, entered_otp):
                         # If OTP is valid, show success message
@@ -63,6 +66,10 @@ class UserLoginSystem:
                 messagebox.showinfo("Success", "Login successful!")
         else:
             messagebox.showerror("Error", "Invalid username or password")
+            
+        # If user doesn't have 2FA set up, provide an option to enable it
+        if not otp_secret and messagebox.askyesno("Enable 2FA", "Do you want to enable 2FA?"):
+            self.enable_2fa(username)
 
     # Generate a QR code for the OTP secret
     def generate_qr_code(self, otp_secret):
@@ -121,6 +128,137 @@ class UserLoginSystem:
         data = {'username': username}
         response = requests.post(url, json=data)
         return response.json()
+
+    def create_user(self):
+        username = simpledialog.askstring("Create User", "Enter new username:")
+        if not username:
+            return  # User clicked Cancel
+
+        # Check if the username already exists
+        users = self.load_users()
+        if username in users:
+            messagebox.showerror("Error", "Username already exists")
+            return
+
+        # Get password and confirm password from user
+        password = simpledialog.askstring("Create User", "Enter password:", show="*")
+        if not password:
+            return  # User clicked Cancel
+
+        confirm_password = simpledialog.askstring("Create User", "Confirm password:", show="*")
+        if password != confirm_password:
+            messagebox.showerror("Error", "Passwords do not match")
+            return
+
+        # Add new user to the dictionary
+        users[username] = {"password": password, "secret_key": None, "email": None}
+
+        # Save the updated user dictionary to the JSON file
+        self.save_users(users)
+
+        enable_2fa = messagebox.askyesno("Enable 2FA", "Do you want to enable 2FA?")
+        email = None
+        secret_key = None
+
+        # If 2FA is enabled, generate dynamic secret key from the server
+        if enable_2fa:
+            response = self.generate_dynamic_secret_key_from_server(username)
+            if 'error' not in response:
+                secret_key = response.get('secret_key')
+                if secret_key:
+                    self.generate_qr_code(secret_key)
+                    email = simpledialog.askstring("Enable 2FA", "Enter your email for recovery:")
+                else:
+                    messagebox.showerror("Error", "Failed to generate secret key.")
+
+        # Update the user dictionary with the generated secret key and email
+        users[username]["secret_key"] = secret_key
+        users[username]["email"] = email
+
+        # Save the updated user dictionary to the JSON file again
+        self.save_users(users)
+
+        messagebox.showinfo("Success", "User created successfully!")
+        
+    def save_users(self, users):
+        with open(self.users_file, "w") as file:
+            json.dump(users, file)
+
+    def enable_2fa(self, username):
+        # Make a request to the server to generate the dynamic secret key
+        response = self.generate_dynamic_secret_key_from_server(username)
+        if 'error' not in response:
+            secret_key = response['secret_key']
+
+            # Update the user's secret key in the dictionary
+            users = self.load_users()
+            users[username]["secret_key"] = secret_key
+            self.save_users(users)
+
+            # Display the new QR code
+            self.generate_qr_code(secret_key)
+
+            # Optionally, allow the user to input their email address for recovery
+            email = simpledialog.askstring("Enable 2FA", "Enter your email for recovery:")
+            if email:
+                # Update the user's email in the dictionary
+                users[username]["email"] = email
+                self.save_users(users)
+
+            messagebox.showinfo("Success", "2FA enabled successfully!")
+        else:
+            messagebox.showerror("Error", response['error'])
+
+    def generate_dynamic_secret_key_from_server(self, username):
+        url = 'http://127.0.0.1:5000/generate_secret_key'
+        data = {'username': username}  # Pass the username in the request data
+        response = requests.post(url, json=data)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return {'error': f'Error generating secret key: {response.status_code}'}
+
+    def generate_and_display_key(self):
+        # Get the username and email from the user
+        username = simpledialog.askstring("Generate New Key", "Enter your username:")
+        if not username:
+            return  # User clicked Cancel
+
+        # Ensure the username exists
+        users = self.load_users()
+        if username not in users:
+            messagebox.showerror("Error", "Username not found")
+            return
+
+        email = users[username].get("email")
+        if not email:
+            messagebox.showerror("Error", "No email address associated with the account")
+            return
+
+        # Get the email from the user
+        entered_email = simpledialog.askstring("Generate New Key", "Enter your email:")
+        if not entered_email or entered_email != email:
+            messagebox.showerror("Error", "Invalid email address")
+            return
+
+        # Generate a new secret key
+        response = self.generate_dynamic_secret_key_from_server(username)
+        if 'error' in response:
+            messagebox.showerror("Error", response['error'])
+            return
+
+        new_secret_key = response.get('secret_key')
+        if not new_secret_key:
+            messagebox.showerror("Error", "Failed to generate new secret key.")
+            return
+
+        # Update the user's secret key in the dictionary
+        users[username]["secret_key"] = new_secret_key
+        self.save_users(users)
+
+        # Generate a new QR code and display it
+        self.generate_qr_code(new_secret_key)
 
 if __name__ == "__main__":
     app = UserLoginSystem()
--- server.py
+++ server.py
@@ -3,17 +3,27 @@ from flask import Flask, request, jsonify
 import json
 import pyotp
 import secrets  
+import base64
 
 app = Flask(__name__)
 
+# Function to load users from the JSON file
+def load_users():
+    with open(users_file, "r") as file:
+        users_data = json.load(file)
+    return users_data
+
 # Load users from the JSON file
 users_file = "users.json"
-with open(users_file, "r") as file:
-    users_data = json.load(file)
+users_data = load_users()
 
 # Generate a dynamic secret key using a secure random method
 def generate_dynamic_secret_key():
-    return secrets.token_hex(16)
+    # Generate a random byte string
+    random_bytes = secrets.token_bytes(10)
+    # Encode the byte string in base32
+    secret_key = base64.b32encode(random_bytes).decode('utf-8')
+    return secret_key
 
 # Method to get the 2fa key
 @app.route('/get_2fa_key', methods=['POST'])
@@ -21,23 +31,65 @@ def get_2fa_key():
     data = request.get_json()
     username = data.get('username', None)
 
+    app.logger.info(f'Received request for 2FA key from user: {username}')
+
+    # Reload users_data from the file
+    users_data = load_users()
+
     # Check if the user already has a secret key
     if username in users_data:
+        app.logger.info(f'User {username} found. Checking for an existing secret key.')
+
         # If not, generate and assign a new dynamic secret key
         if 'secret_key' not in users_data[username]: 
+            app.logger.info(f'Generating a new secret key for user: {username}')
             users_data[username]['secret_key'] = generate_dynamic_secret_key()
 
         # Use the dynamic secret key for TOTP generation
         secret_key = users_data[username]["secret_key"]
         totp = pyotp.TOTP(secret_key)
         current_code = totp.now()
+
+        # Save the updated user dictionary to the JSON file
+        with open(users_file, "w") as file:
+            json.dump(users_data, file)
+
+        app.logger.info(f'Successfully generated 2FA key for user: {username}')
         return jsonify({'secret_key': secret_key, 'current_code': current_code})
 
+    app.logger.error(f'User not found: {username}')
     return jsonify({'error': 'User not found'})
+
+# Route to generate a new dynamic secret key
+@app.route('/generate_secret_key', methods=['POST'])
+def generate_secret_key():
+    data = request.get_json()
+    username = data.get('username', None)
+
+    app.logger.info(f'Received request to generate a secret key for user: {username}')
+
+    if username is None:
+        app.logger.error('Username not provided in the request.')
+        return jsonify({'error': 'Username not provided'})
+
+    secret_key = generate_dynamic_secret_key()
+
+    # Reload users_data from the file
+    users_data = load_users()
+
+    # Check if the user already exists in the dictionary
+    if username in users_data:
+        app.logger.info(f'User {username} found. Generating a new secret key.')
+        # Save the updated user dictionary to the JSON file
+        with open(users_file, "w") as file:
+            users_data[username]['secret_key'] = secret_key
+            json.dump(users_data, file)
+
+        app.logger.info(f'Successfully generated a secret key for user: {username}')
+        return jsonify({'secret_key': secret_key})
+    else:
+        app.logger.error(f'User not found: {username}')
+        return jsonify({'error': 'User not found'})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
-
-# Update the users.json file with the changes
-with open(users_file, "w") as file:
-    json.dump(users_data, file)
